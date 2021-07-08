0a5468928bf84eb0272f3249165f52ea5246c328
Murat <muratalkanw@hotmail.com>
2021-07-08 07:23:09 +0000

    Add swal2 alert for disk info

--- views/components/functions.blade.php
+++ views/components/functions.blade.php
@@ -6,7 +6,7 @@
             $(this).find('form')[0].reset();
         }
     });
-
+    
     function resetModalForm(modal){
         $(modal).on('hidden.bs.modal', function(e){
             $(this).find('input').val("");
@@ -25,4 +25,37 @@
         $(modal).find('input[name="'+input+'"]').val(value)
     }
 
+    function getDiskAlert(title, formData, route){
+        showSwal('{{__("Yükleniyor...")}}','info');
+        request(`{{API('${route}')}}`, formData, function(response) {
+            const data = JSON.parse(response).message;
+            color = data.DirectoryStatus === '1' ? 'success' : 'danger';
+            Swal.close();
+            Swal.fire({
+                title: `<h5><span class='badge badge-${color} badge-pill'>'${title}'</span></h5>`,
+                width: '550px',
+                html:
+                    `
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Bağlanılan Yer")}}:</strong><span class='badge badge-pill'>${data.MountedOn}</span> </li>
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Dosya Sistemi")}}:</strong><span class='badge badge-pill'>${data.Filesystem}</span> </li>
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Tür")}}:</strong><span class='badge badge-pill'>${data.Type}</span> </li>
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Toplam Boyut")}}:</strong><span class='badge badge-secondary badge-pill'>${data.Size}</span> </li>
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Kullanılan")}}:</strong><span class='badge badge-danger badge-pill'>${data.Used} (${data.UsedPercentage})</span> </li>
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Dosya Boyutu")}}:</strong><span class='badge badge-warning badge-pill'>${data.InstallSize}</span> </li>
+                    <li class='list-group-item d-flex justify-content-between align-items-center'><strong>{{__("Boş")}}:</strong><span class='badge badge-primary badge-pill'>${data.Available}</span> </li>
+                    `,
+                showCancelButton: true,
+                showConfirmButton: true,
+                confirmButtonText: "{{__('Yenile')}}", cancelButtonText: "{{__('Kapat')}}"
+            }).then((result) => {
+                if (result.value) {
+                    getDiskAlert(title, formData, route);
+                }
+        });
+        },function(response) {
+            const error = JSON.parse(response).message;
+            Swal.fire("{{ __('Error!') }}", error, "error");
+        });
+    }
+    
 </script>
